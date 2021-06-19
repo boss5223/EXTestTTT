71f0f9d01d3ede387450dea398e660f6cd86113a
boss5223 <deathdreamerlive@gmail.com>
2021-06-19 17:44:53 +0700

    commit

---
+++ Tic-Tac-Toe/Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public string playerTurn;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void SetTurnPlayer(string player)
+    {
+        playerTurn = player;
+    }
+}
---
+++ Tic-Tac-Toe/Assets/Scripts/GridSpace.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridSpace : MonoBehaviour
+{
+    public Button gridButton;
+    public Text gridText;
+    public string player;
+    private GridSpace grid;
+
+    private void Start()
+    {
+        gridButton.onClick.AddListener(() => SetGridSpace());
+    }
+    public GridSpace SetGridSpace()
+    {
+        player = GameManager.instance.playerTurn;
+        gridText.text = player;
+        gridButton.interactable = false;
+
+        return this;
+    }
+}
